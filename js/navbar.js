e45da5a129543bfcd7e1ae54878822cf80776142
Jied Joshua <jjagcastillo@mymail.mapua.edu.ph>
2023-05-12 17:36:02 +0800

    update2

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+<script>
+$(document).ready(function() {
+  // Add a click event to the hamburger button
+  $(".navbar-toggler").click(function() {
+    // Toggle the visibility of the navigation menu
+    $("#navbarNav").toggle();
+  });
+});
+</script>
